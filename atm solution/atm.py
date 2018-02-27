69c138cc0ae8e822f3818f8be6f250b04ce7ff64
Walid Zakaria <36845491+walidpiano@users.noreply.github.com>
2018-02-27 09:05:38 +0000

    Workshop Lesson 7: Multiple withdrawal with deduction from main balance
    I put the withdrawal in a function that returns the amount left.

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -1,32 +1,45 @@
-# ATM program
-# banknotes: 100, 50, 10, 5, 2, 1
+  # allowed papers: 100, 50, 10, 5, and cents
 
-Available = 500
+def withdraw(balance, request):
+  print 'current balance is ' + str(balance)
+  if request > balance:
+    print("Can't give you all this money !!")
+    return balance
 
-
-def withdraw(request):
-  if request > Available:
-    print 'The entered amount is more than the vailable credit!'
   elif request <= 0:
-    print 'You have entered invalid amount!'
+    print("More than zero plz!")
+    return balance
+
   else:
-    while request != 0:
+    
+    #I made this variable because the variable request gets decreased inside the while loop
+    deducted_money = request
+    while request > 0:
+
       if request >= 100:
         request -= 100
-        print 'Give 100'
+        print("give 100")
+
       elif request >= 50:
         request -= 50
-        print 'Give 50'
+        print("give 50")
+
       elif request >= 10:
         request -= 10
-        print 'Give 10'
+        print("give 10")
+
       elif request >= 5:
         request -= 5
-        print 'Give 5'
-      else:
-        print 'Give ' + str(request)
+        print("give 5")
+
+      elif request < 5:
+        print("give " + str(request))
         request = 0
-        
+    return (balance - deducted_money)
 
+balance = 500
 
-withdraw(277)
+balance = withdraw(balance, 277)
+balance = withdraw(balance, 30)
+balance = withdraw(balance, 5)
+balance = withdraw(balance, 500)
