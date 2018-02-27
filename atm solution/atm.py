b6ebf25980042e63f2ee4a6d98a6e34632b93a75
Walid Zakaria <36845491+walidpiano@users.noreply.github.com>
2018-02-27 08:27:34 +0000

    Considering Invalid amount & modifying  variable name
    - If amount is less than or equal 0, then the functions prints error message.
    - I have amended the variable name to be readable.

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -4,26 +4,28 @@
 Available = 500
 
 
-def withdraw(reqAmount):
-  if reqAmount > Available:
+def withdraw(request):
+  if request > Available:
     print 'The entered amount is more than the vailable credit!'
+  elif request <= 0:
+    print 'You have entered invalid amount!'
   else:
-    while reqAmount != 0:
-      if reqAmount >= 100:
-        reqAmount -= 100
+    while request != 0:
+      if request >= 100:
+        request -= 100
         print 'Give 100'
-      elif reqAmount >= 50:
-        reqAmount -= 50
+      elif request >= 50:
+        request -= 50
         print 'Give 50'
-      elif reqAmount >= 10:
-        reqAmount -= 10
+      elif request >= 10:
+        request -= 10
         print 'Give 10'
-      elif reqAmount >= 5:
-        reqAmount -= 5
+      elif request >= 5:
+        request -= 5
         print 'Give 5'
       else:
-        print 'Give ' + str(reqAmount)
-        reqAmount = 0
+        print 'Give ' + str(request)
+        request = 0
         
 
 
