ac70391423f09aba91261838260880d0e529d147
Walid Zakaria <36845491+walidpiano@users.noreply.github.com>
2018-02-27 15:51:45 +0000

    Creating an ATM class to work for lesson 9

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -1,43 +1,55 @@
-  # allowed papers: 100, 50, 10, 5, and cents
+class ATM:
+  
+  def __init__(self, balance, bank_name):
+    self.balance = balance
+    self.bank_name = bank_name
+  
+  def withdraw(self, request):
+    print 'Wecome to ' + self.bank_name
+    print 'current balance = ' + str(self.balance)
+    print '=================================='
+    if request > self.balance:
+      print("Can't give you all this money !!")
+  
+    elif request <= 0:
+      print("More than zero plz!")
+  
+    else:
+      self.balance -= request
+      while request > 0:
+  
+        if request >= 100:
+          request -= 100
+          print("give 100")
+  
+        elif request >= 50:
+          request -= 50
+          print("give 50")
+  
+        elif request >= 10:
+          request -= 10
+          print("give 10")
+  
+        elif request >= 5:
+          request -= 5
+          print("give 5")
+  
+        elif request < 5:
+          print("give " + str(request))
+          request = 0
+    print '=================================='  
+    return self.balance
 
-def withdraw(balance, request):
-
-  print 'current balance is ' + str(balance)
-  if request > balance:
-    print("Can't give you all this money !!")
-
-  elif request <= 0:
-    print("More than zero plz!")
-
-  else:
-    balance -= request
-    while request > 0:
-
-      if request >= 100:
-        request -= 100
-        print("give 100")
-
-      elif request >= 50:
-        request -= 50
-        print("give 50")
-
-      elif request >= 10:
-        request -= 10
-        print("give 10")
-
-      elif request >= 5:
-        request -= 5
-        print("give 5")
-
-      elif request < 5:
-        print("give " + str(request))
-        request = 0
-    
-  return balance
 
 
-balance = 500
-balance = withdraw(balance, 277)
-balance = withdraw(balance, 30)
-balance = withdraw(balance, 5)
-balance = withdraw(balance, 500)
+balance1 = 500
+balance2 = 1000
+
+atm1 = ATM(balance1, "Smart Bank")
+atm2 = ATM(balance2, "Baraka Bank")
+
+atm1.withdraw(277)
+atm1.withdraw(800)
+
+atm2.withdraw(100)
+atm2.withdraw(2000)
