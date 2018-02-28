c87964eccf2a00b10036cdd37e16c9669dedf2d0
Walid Zakaria <36845491+walidpiano@users.noreply.github.com>
2018-02-28 09:26:35 +0000

    Applying SRP (single responsibility principle)
    I created a new function for processing request (process_request)

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -16,30 +16,33 @@ class ATM:
   
     else:
       self.balance -= request
-      while request > 0:
-  
-        if request >= 100:
-          request -= 100
-          print("give 100")
-  
-        elif request >= 50:
-          request -= 50
-          print("give 50")
-  
-        elif request >= 10:
-          request -= 10
-          print("give 10")
-  
-        elif request >= 5:
-          request -= 5
-          print("give 5")
-  
-        elif request < 5:
-          print("give " + str(request))
-          request = 0
+      self.process_request(request)
     print '=================================='  
     return self.balance
+  
+  @staticmethod
+  def process_request(request):
+    while request > 0:
+  
+      if request >= 100:
+        request -= 100
+        print("give 100")
 
+      elif request >= 50:
+        request -= 50
+        print("give 50")
+
+      elif request >= 10:
+        request -= 10
+        print("give 10")
+
+      elif request >= 5:
+        request -= 5
+        print("give 5")
+
+      elif request < 5:
+        print("give " + str(request))
+        request = 0
 
 
 balance1 = 500
